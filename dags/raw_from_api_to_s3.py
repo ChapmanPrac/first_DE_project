cafad2b84359e54e3cc14dad7df55dc036e42e8d
Chapman <chapmankphoenix@gmail.com>
2025-09-19 22:02:18 +0300

    Добавили в сборку duckdb

--- dags/raw_from_api_to_s3.py
+++ dags/raw_from_api_to_s3.py
@@ -28,7 +28,7 @@ SHORT_DESCRIPTION = "SHORT DESCRIPTION"
 args = {
     "owner": OWNER,
     "start_date": pendulum.datetime(2025, 5, 1, tz="Europe/Moscow"),
-    "catchup": True,
+    "catchup": False,
     "retries": 3,
     "retry_delay": pendulum.duration(hours=1),
 }
